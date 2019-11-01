7a695cde0b7774bd3c81ed9c15e28cb602b9dc08
Dwyane <dwyanele@163.com>
2019-11-01 19:32:15 +0800

    add-goods

--- EvilShoes/commodity/views.py
+++ EvilShoes/commodity/views.py
@@ -1,3 +1,39 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from commodity.models import *
+
+
+def GoodsInfo(request):
+    if request.method == 'GET':
+        try:
+            goods = Commodity.objects.all()
+            a = []
+            for good in goods:
+                b = {}
+                b['id'] = good.id
+                b['name'] = good.name
+                b['shelves'] = good.shelves
+                b['price'] = str(good.price)
+                b['description'] = good.description
+                b['images'] = good.images
+                a.append(b)
+            return JsonResponse({'code': 200, 'data': a})
+        except Exception as e:
+            return JsonResponse({'code': 403, 'error': 'The force majeure cause error'})
+
+
+def GoodsType(request):
+    if request.method == 'GET':
+        try:
+            gts = Classify.objects.all()
+            c = []
+            for gt in gts:
+                d = {}
+                d['name'] = gt.name
+                d['description'] = gt.description
+                c.append(d)
+            return JsonResponse({'code': 200, 'data': c})
+        except Exception as e:
+            return JsonResponse({'code': 403, 'error': 'The force majeure cause error'})
