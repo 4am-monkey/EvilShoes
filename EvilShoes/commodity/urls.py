bc5a00fc479f9ca6c282f76afc245063d7acae51
Dwyane <dwyanele@163.com>
2019-11-05 10:21:03 +0800

    change commodity/urls

--- EvilShoes/commodity/urls.py
+++ EvilShoes/commodity/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from commodity import views
 
 urlpatterns = [
+    # 127.0.0.1:8000/commodity
     url(r'^$', views.GoodsInfo),
-    url(r'^(?P<classifyid>\w+)$', views.GoodsType),
+    # 127.0.0.1:8000/commodity/classify
+    url(r'^classify$', views.GoodsType),
 ]
