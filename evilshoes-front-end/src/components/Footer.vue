4883eaeb6d5c3a6f0316a1aebda96671b13f6ac8
gaoyt <gaoyt226@163.com>
2019-11-14 15:55:24 +0800

    footer

--- evilshoes-front-end/src/components/Footer.vue
+++ evilshoes-front-end/src/components/Footer.vue
@@ -1,5 +1,27 @@
 <template>
-    <div class="x-header">
-        底部
+    <div class="x-footer">
+        <div>
+            2019-2019 4am-monkey版权所有
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+  .x-footer div{
+      /* border: solid 1px red; */
+      background: black;
+      width: 100%;
+      margin-top: 100px;
+      height: 100px;
+      color: white;
+      text-align: center;
+      line-height: 100px;
+      font-size: 14px;
+  }
+</style>
